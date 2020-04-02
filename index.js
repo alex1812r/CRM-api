0043c55408910c9f38f78aad6ea42f52ad538e84
Alexander <alex1812r@gmail.com>
2020-04-02 13:04:49 -0400

    varibales de entorno

--- index.js
+++ index.js
@@ -4,10 +4,11 @@ const routes = require('./routes');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+require('dotenv').config({ path: 'variables.env' });
 
 // CONECTAR MONGOOSE
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost/CRM', {
+mongoose.connect(process.env.DB_URL, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
@@ -18,9 +19,18 @@ mongoose.connect('mongodb://localhost/CRM', {
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+// CORS
+// LISTA DE DOMINIOS PERMITIDOS
+const listaBlanca = [process.env.FRONTEND_URL];
 // HABILITAR CORS
 app.use(cors({
-  origin: 'http://localhost:3000'
+  origin: (origin, callback) => {
+    const existe = listaBlanca.some(dominio => dominio === origin);
+    if(existe)
+      callback(null, true);
+    else
+      callback((new Error('No permitido por CORS')));
+  }
 }));
 
 // USAR RUTAS
@@ -29,7 +39,10 @@ app.use('/', routes());
 // CARPETA PUBLICA
 app.use(express.static('uploads'));
 
+const host = process.env.HOST || '0.0.0.0';
+const port = process.env.PORT || 5000;
+
 // INICIAR APP(SERVIDOR)
-app.listen(5000, (req, res) => {
+app.listen(port, host, (req, res) => {
   console.log('SERVIDOR INICIADO EN PUERTO : ', 5000);
 });
